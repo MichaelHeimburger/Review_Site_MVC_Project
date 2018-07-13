81886cff6aadc49b2292a45956e92e537761e6a6
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-13 12:03:54 -0400

    First commit

---
+++ ReviewSiteMVC/Models/CityCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteMVC.Models
+{
+    public class CityCategory
+    {
+        [Key]
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ ReviewSiteMVC/Models/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteMVC.Models
+{
+    public class Review
+    {
+
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewDescription { get; set; }
+        [Range(1, 101)]
+        public int Rating { get; set; }
+        public string ReviewerName { get; set; }
+
+        [ForeignKey("CityCategory")]
+        public int CityID { get; set; }
+        public virtual CityCategory CityCategory { get; set; }
+
+
+
+
+
+
+    }
+}
